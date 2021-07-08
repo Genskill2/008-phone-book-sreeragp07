f357e82c164425f3799741a9dfc984b288228c7f
sreeragp07 <84373517+sreeragp07@users.noreply.github.com>
2021-07-08 17:52:54 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -62,7 +62,21 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    printf("NOT IMPLEMENTED!\n"); /* TBD  */
+    //printf("NOT IMPLEMENTED!\n"); 
+    if (argc != 3) {
+      print_usage("Improper arguments for search", argv[0]);
+      exit(1);
+      }
+    char *name = argv[2];
+    FILE *fp = open_db_file();
+    if (!search(fp, name)) {
+      printf("no match\n");
+      fclose(fp);
+      exit(1);
+    }
+    
+    
+    /* TBD  */
   } else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
     if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
@@ -94,7 +108,13 @@ FILE *open_db_file() {
   
 void free_entries(entry *p) {
   /* TBD */
-  printf("Memory is not being freed. This needs to be fixed!\n");  
+  entry * tmp;
+  while(p != NULL){
+    tmp = p->next;
+    free(p);
+    p = tmp;
+  }
+  //printf("Memory is not being freed. This needs to be fixed!\n");  
 }
 
 void print_usage(char *message, char *progname) {
@@ -131,20 +151,17 @@ entry *load_entries(FILE *fp) {
   entry *tmp = NULL;
   /* Description of %20[^,\n]
      % is the start of the specifier (like %s, %i etc.)
-
      20 is the maximum number of characters that this will take. We
         know that names and phone numbers will be 20 bytes maximum so
         we limit it to that. %20s will read in 20 character strings
         (including the , to separate the name and phone number. That's
         why we use
-
     [^,\n] Square brackets are used to indicate a set of allowed
            characters [abc] means only a, b, or c. With the ^, it's
            used to specify a set of disallowed characters. So [^abc]
            means any character *except* a, b, or c. [^,] means any
            character except a , [^,\n] means any character except a
            comma(,) or a newline(\n).
-
     %20[^,\n] will match a string of characters with a maximum length
      of 20 characters that doesn't have a comma(,) or a newline(\n).
   */        
@@ -178,12 +195,36 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count=0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
+    count++;
+    p=p->next;
+  }
+  
+  printf("Total entries :  %d\n",count);
+  /* TBD print total count */
+  free_entries(base);
+}
+
+
+int search(FILE *db_file,char *name) {
+  entry *p = load_entries(db_file);
+  entry *base = p;
+  int result=0;
+  while (p!=NULL) {
+    //printf("%-20s : %10s\n", p->name, p->phone);
+    if (strcmp(p->name, name) == 0)
+    {
+    	printf("%-20s : %10s\n", p->name, p->phone);
+    	result=1;
+    	break;
+    }
     p=p->next;
   }
   /* TBD print total count */
   free_entries(base);
+  return result;
 }
 
 
@@ -193,7 +234,13 @@ int delete(FILE *db_file, char *name) {
   entry *prev = NULL;
   entry *del = NULL ; /* Node to be deleted */
   int deleted = 0;
+  //prev=p;
+  if (strcmp(p->name, name) == 0) {
+  	base=p->next;
+  }
+  
   while (p!=NULL) {
+    prev=p;
     if (strcmp(p->name, name) == 0) {
       /* Matching node found. Delete it from the linked list.
          Deletion from a linked list like this
@@ -205,10 +252,20 @@ int delete(FILE *db_file, char *name) {
          
          If the node to be deleted is p0, it's a special case. 
       */
+     
+     del=p;
+     prev->next=del->next;
+    
+    
+     
+      
 
       /* TBD */
     }
+   
+    p=p->next;
   }
+  
   write_all_entries(base);
   free_entries(base);
   return deleted;
